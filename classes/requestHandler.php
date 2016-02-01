8e4de46023b48b9d9ef5a2e1a0bd8575ba280d7d
Carl Turechek <carl@siteraiser.com>
2016-02-01 12:53:39 -0500

    Added a simple routing function

--- classes/requestHandler.php
+++ classes/requestHandler.php
@@ -1,6 +1,65 @@
 <?php 
 abstract class helpers{
 
+	public function routes($rows,$path){
+
+		foreach($rows as $key => $value){	
+			$routes[]=['pattern' => $key,'controller' => $value];
+		}
+		unset($value);
+
+		function match($pattern,$urlSegs){
+			$i = 0;$match = false; $pattcount=count($pattern);$urlcount=count($urlSegs);
+			foreach($pattern as $value){				
+				if($value != '(:any)'){
+					if($urlSegs[$i] == $value){
+						$match = true;
+					}else{
+						return false;
+					}
+				}else{
+					if(isset($urlSegs[$i])){
+						$any[$i]=$urlSegs[$i];
+					}
+				}
+				$i++;
+				if($urlcount-- == 0){return false;}
+			}
+			if(!isset($any) && $match==true){
+				$any=true;
+			}
+			return $any;
+		}
+
+		$i = 0;
+		foreach($routes as $value){	
+			$pattern=explode('/',$value['pattern']);
+			$urlSegs = explode('/', $path,count($pattern));
+			$results = match($pattern,$urlSegs);
+			if($results){
+				break;
+			}
+			$i++;
+		}
+
+		if($results !==true && !empty($results)){
+			$k=1;
+			foreach($results as $value){
+				$trans[]=$value;
+			}
+		}
+		$controller_method=[];
+		if(isset($routes[$i]['controller'])){
+			$split = explode('$1', $routes[$i]['controller']);
+			$controller_method = $split[0];
+			$controller_method = explode('/',$controller_method);
+		}
+
+		if(isset($trans)){
+			$controller_method[2]=$trans;
+		}
+		return $controller_method;
+	}
 	public function get_include_contents($filename,$data) {
 		foreach($data as $key => $value){
 			$$key = $value;
@@ -23,7 +82,6 @@ abstract class helpers{
 	
 	public function loadModel($path) {
 		$name =	end(explode('/',$path));
-		//echo'loading model: '.$name;
 		$loadname = 'models/'.$path.'.php';
 		if(file_exists($loadname) AND !isset($this->$name)){
 			include_once($loadname);
@@ -32,7 +90,6 @@ abstract class helpers{
 			//add pdo 
 			include_once('secure/db.inc.php');		
 			if(!is_object($this->pdo)){
-				//echo'connected -';
 				try {
 					$this->pdo = new PDO('mysql:host=localhost;dbname='.$database, $username, $password);    					
 				} catch (PDOException $e) {
@@ -66,6 +123,8 @@ abstract class helpers{
 		$string = iconv('UTF-8', 'ASCII//TRANSLIT', $string);   
 		$string = preg_replace('/^a-z0-9\-\_]/i', '', $string);
 		$string = str_replace(' ', '-', $string);
+		$string = str_replace('(', '', $string);
+		$string = str_replace(')', '', $string);
 		$string = trim($string, '-');
 		$string = strtolower($string);
 
@@ -89,8 +148,7 @@ abstract class helpers{
 				}
 			$out.=' value="'.$field.'">';
 			$out.=$field . '</option>';  
-			$allOut.= $out;
-			
+			$allOut.= $out;			
 		 }
 		 return $allOut.'</select>';
 	}	
@@ -116,17 +174,9 @@ class requestHandler extends helpers{
 	
 	public function getContent(){
 		$this->base_url    = 'http://localhost/';
-		$this->doc_root    = $_SERVER['DOCUMENT_ROOT'].'/';
-		
+		$this->doc_root    = $_SERVER['DOCUMENT_ROOT'].'/';		
 		$this->req_url     = parse_url(urldecode($_SERVER['REQUEST_URI']), PHP_URL_PATH);
-		//$query_str = parse_url($_SERVER['REQUEST_URI'], PHP_URL_QUERY);
-		//echo$query_str;
-		//parse_str($query_str, $_REQUEST);
-		//parse_str($query_str, $_GET);
-		//$requested_img  = basename($requested_uri);
-	
-		$this->output="";	//set default out html
-		
+		$this->output="";	//set default out html		
 		$this->path = trim($this->req_url, '/');    // Trim leading slash(es)
 	
 		if($this->path == null || $this->path == 'index.php'){   // No path url_segments means home
@@ -135,34 +185,50 @@ class requestHandler extends helpers{
 		
 		}else
 		{
-			if(substr($this->path,-4) == '.xml'){		
-				$this->path=substr($this->path,0,strlen($this->path)-4);
-			}
-			
 			$this->url_segments = explode('/', urldecode($this->path));			
 			$this->path=parse_url($this->path, PHP_URL_PATH);	//echo path here		
 		}
-		
 		$this->controllername=$this->url_segments[0];	
+		if(isset($this->url_segments[1])){
+			$method=$this->url_segments[1];
+		}
+		//Routing		
+		$rows['sitemap.xml'] = 'sitemap';
+		$rows['link1/(:any)'] = 'controller/method/$1';
+		$rows['link2/(:any)'] = 'controllera/methoda/$1';
+		$rows['link3/(:any)/links/more/(:any)'] = 'controllera/methoda/$1/$2';
+		
+		$controller_method=$this->routes($rows,$this->path);
+		unset($rows);
+		if(isset($controller_method[0])){
+			$this->controllername = $controller_method[0];
+		}
+		
+		if(isset($controller_method[1])){
+			$method = $controller_method[1];
+		}
+		$params =[];
+		if(isset($controller_method[2])){	
+			$params = $controller_method[2];
+		}
+		array_unshift($params, $this->path);
 		
 		$controllerpath='controllers/'.$this->controllername.'.php';
 
 		if(file_exists($controllerpath)){
 			include_once($controllerpath);
 			$controller = new $this->controllername();	
-			if(isset($this->url_segments[1]) AND method_exists($controller, $this->url_segments[1])){
-				$function = $this->url_segments[1];
+			if(isset($method) AND method_exists($controller, $method)){
+				$function = $method;
 				//call function set view data
-				$this->add_props($controller);
-				
-				$controller->$function($this->path);
+				$this->add_props($controller);				
+				call_user_func_array(array($controller,$function), $params);
 				$this->output=$controller->output;
 				
 			} else if(method_exists($controller, 'index')){
 				//call function set view data
 				$this->add_props($controller);
-				
-				$controller->index($this->path);
+				call_user_func_array(array($controller,'index'), $params);
 				$this->output=$controller->output;
 			}else{//maybe add fallback controller for db etc:))
 				$notfound=true;
@@ -173,8 +239,6 @@ class requestHandler extends helpers{
 	   if(isset($notfound)){$this->notFound();}
 	}
 	
-	
-
 	
 	public function notFound(){
 		header('HTTP/1.1 404 Not Found');
